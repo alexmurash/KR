3f418663c103fd1a4b19948825ed879a0d0151f1
root <root@kali.localdomain>
2025-03-11 15:21:47 +0300

    Init commit: ager

---
+++ ager.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+name = input("Введите ваше имя: ")
+age = int(input("Введите ваш возраст: "))
+age_in_10_years = age + 10
+print(f"Привет, {name}!")
+print(f"Через 10 лет вам будет {age_in_10_years} лет.")
